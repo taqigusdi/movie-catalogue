6572068a832386855e4f4bd6570f090ea17de08c
Taqiy <taqigusdi@gmail.com>
2022-10-18 02:18:02 +0700

    Add dotenv-webpack and reconfigure webpack and karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,8 @@
 // Karma configuration
 // Generated on Fri Jul 03 2020 20:15:52 GMT+0700 (Western Indonesia Time)
+const Dotenv = require('dotenv-webpack');
+const path = require('path');
+
 module.exports = function (config) {
   config.set({
 
